10d2208c36d741537d2dcaf0c330b5b858832969
Denvhel <dennisvanhelvoort@air-van-dennis.home>
2024-01-18 21:50:06 +0100

    Added loop to play 3 rounds. Also comparison values with output who wins

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const choices = ["Rock", "Paper", "Scissor"];
+const choices = ["rock", "paper", "scissor"];
 
 function getComputerChoice() {
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -12,13 +12,31 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
-function singleRound() {
-    if (getPlayerChoice === getComputerChoice) {
+for (let i = 0; i < 3; i++) {
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
         console.log("It's a tie!");
+    } 
+    else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("Computer wins!");
     }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("Player wins!");
+    }
+    else if (playerSelection === "scissor" && computerSelection === "paper") {
+        console.log("Player wins!");
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissor") {
+        console.log("Computer wins!");
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissor") {
+        console.log ("Player wins!");
+    }
+    else if (playerSelection === "scissor" && computerSelection === "rock") {
+        console.log ("Computer wins!");
+    }
+
 }
-
-getPlayerChoice();
-getComputerChoice();
-singleRound();
-
